a63f8ee6d2274e0ace8acdd84638abe6d14bf5a6
tjreed15 <tjreed15@gmail.com>
2014-03-14 15:22:10 -0700

    Edited the loading text style to get rid of borders etc.

--- sweets/categories.inc.php
+++ sweets/categories.inc.php
@@ -7,7 +7,7 @@
 ?>	
 		<div id=<?php echo $category."Rect";?> class="rect row1">
 			<div id="<?php echo $category;?>SignSquare" class="signSquare">
-				<img src="sweets/signPics/<?php echo $category?>Sign.png" />
+				<img src="sweets/signPicsSmall/<?php echo $category?>Sign.png" />
 			</div>
 			<div id="<?php echo $category;?>PicSquare" class="picSquare">
 				<div class="button">
@@ -15,7 +15,7 @@
 					<div class="outButton"><img src="sweets/button.png" /></div>
 				</div>
 				<div class="buttonPhoto">
-					<img src="sweets/buttonPics/<?php echo $category?>.png" />
+					<img src="sweets/buttonPicsSmall/<?php echo $category?>.png" />
 				</div>
 				<div id="<?php echo $category;?>Bite" class="bite" style="top:0;"><img src="sweets/bite3.png" /></div>
 			</div>
@@ -33,12 +33,12 @@
 					<div class="outButton"><img src="sweets/button.png" /></div>
 				</div>
 				<div class="buttonPhoto">
-					<img src="sweets/buttonPics/<?php echo $category?>.png" />
+					<img src="sweets/buttonPicsSmall/<?php echo $category?>.png" />
 				</div>
 				<div id="<?php echo $category;?>Bite" class="bite" style="bottom:0;"><img src="sweets/bite4.png" /></div>
 			</div>
 			<div id="<?php echo $category;?>SignSquare" class="signSquare">
-				<img src="sweets/signPics/<?php echo $category?>Sign.png" />
+				<img src="sweets/signPicsSmall/<?php echo $category?>Sign.png" />
 			</div>
 			<div class="mouseListenerCover coverRow2" onMouseOut="hideSign('<?php echo $category;?>');" onMouseOver="showSign('<?php echo $category;?>');" onClick="showContent('<?php echo $category?>')"></div>
 		</div>
